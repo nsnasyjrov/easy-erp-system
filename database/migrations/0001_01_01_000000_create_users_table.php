a5c95857c971e47ccb9eeaff28e32914ce4c92ed
Nail <nsnasyjrov@gmail.com>
2026-05-25 23:20:43 +0300

    feat: extend user table structure

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->string('login')->unique();
+            $table->string('first_name');
+            $table->string('middle_name');
+            $table->string('last_name');
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
