0bb94e1428e0e01846d826bc63cdfe3f312af78f
Nail <nsnasyjrov@gmail.com>
2026-06-09 23:24:24 +0300

    feat: Added label method to enums.

--- app/Enums/ClientType.php
+++ app/Enums/ClientType.php
@@ -6,4 +6,14 @@ enum ClientType: string
 {
     case Individual = 'individual';
     case Company = 'company';
+
+    public function label(): string
+    {
+
+        return match($this) {
+            self::Individual => 'Физическое лицо',
+            self::Company => 'Контрагент',
+        };
+
+    }
 }
--- app/Enums/ContactInfoType.php
+++ app/Enums/ContactInfoType.php
@@ -11,4 +11,18 @@ enum ContactInfoType: string
     case Website = 'website';
     case Email = 'email';
 
+    public function label(): string
+    {
+
+        return match ($this) {
+            self::Address => 'Фактический адрес',
+            self::LegalAddress => 'Юридический адрес',
+            self::PhoneNumber => 'Номер телефона',
+            self::Telegram => 'Телеграм',
+            self::Website => 'Веб-сайт',
+            self::Email => 'Электронная почта',
+        };
+
+    }
+
 }
