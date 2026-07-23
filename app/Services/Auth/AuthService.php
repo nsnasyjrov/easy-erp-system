9ae0ae24c0397c00e5e2ff00d2836eb923ee8e9e
Nail <nsnasyjrov@gmail.com>
2026-07-23 21:46:29 +0300

    refactor: removed useless method

--- app/Services/Auth/AuthService.php
+++ app/Services/Auth/AuthService.php
@@ -75,34 +75,6 @@ class AuthService
         return $tokensPlainText;
     }
 
-    public function verify(int $id, string $hash)
-    {
-        $user = User::query()->findOrFail($id);
-
-        if(!hash_equals(sha1($user->getEmailForVerification()), $hash)) {
-            return response()->json([
-                'status' => 'error',
-                'message' => 'Invalid verification link'
-            ], 403);
-        }
-
-        if ($user->hasVerifiedEmail()) {
-            return response()->json([
-                'status' => 'warning',
-                'You have already confirmed your email earlier'
-            ]);
-        }
-
-        $user->markEmailAsVerified();
-
-        event(new Verified($user));
-
-        return response()->json([
-            'status' => 'success',
-            'Email address verified successfully.'
-        ]);
-    }
-
     public function forgotPassword(string $email)
     {
         Password::sendResetLink(
