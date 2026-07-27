8555e684c32e34231a7474772ba38c6acb58783a
Nail <nsnasyjrov@gmail.com>
2026-07-27 22:42:31 +0300

    feat(Factory): added new fields

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -25,7 +25,10 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
+            'login' => fake()->unique()->firstName(),
+            'first_name' => fake()->firstName(),
+            'middle_name' => fake()->firstName(),
+            'last_name' => fake()->lastName(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
