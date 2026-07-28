73356844d3cb23d502a13eabbe74784af50b7596
Nail <nsnasyjrov@gmail.com>
2026-07-28 22:16:00 +0300

    feat(factory): added verified method into user factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -45,4 +45,11 @@ class UserFactory extends Factory
             'email_verified_at' => null,
         ]);
     }
+
+    public function verified(): static
+    {
+        return $this->state(fn (array $attributes) => [
+            'email_verified_at' => now(),
+        ]);
+    }
 }
