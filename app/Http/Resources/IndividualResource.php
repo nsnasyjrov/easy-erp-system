4a1060692b6c82c777cd5f0f1446f0fc91023204
Nail <nsnasyjrov@gmail.com>
2026-08-09 01:26:54 +0300

    chore: removed '-'

--- app/Http/Resources/IndividualResource.php
+++ app/Http/Resources/IndividualResource.php
@@ -19,12 +19,12 @@ class IndividualResource extends JsonResource
             "id" => $this->id,
             "first_name" => $this->first_name,
             "middle_name" => $this->middle_name,
-            "last_name-" => $this->last_name,
-            "sex-" => $this->sex,
-            "birth_date-" => $this->birth_date?->toDateTimeString(),
-            "client_id-" => $this->client_id,
-            "created_at-" => $this->created_at?->toDateTimeString(),
-            "updated_at-" => $this->updated_at->toDateTimeString(),
+            "last_name" => $this->last_name,
+            "sex" => $this->sex,
+            "birth_date" => $this->birth_date?->toDateTimeString(),
+            "client_id" => $this->client_id,
+            "created_at" => $this->created_at?->toDateTimeString(),
+            "updated_at" => $this->updated_at->toDateTimeString(),
 
             "client" => new ClientResource($this->whenLoaded('client'))
         ];
