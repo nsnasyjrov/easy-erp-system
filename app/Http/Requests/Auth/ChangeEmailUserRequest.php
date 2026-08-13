9d9aa5e6e4bfacd886f1569b8e0e8a508b137f5c
Nail <nsnasyjrov@gmail.com>
2026-08-13 23:33:16 +0300

    feat(test): duplicateTesting, test_user_cannot_change_password_incorrect, test_registration_requires_field

--- app/Http/Requests/Auth/ChangeEmailUserRequest.php
+++ app/Http/Requests/Auth/ChangeEmailUserRequest.php
@@ -33,17 +33,4 @@ class ChangeEmailUserRequest extends FormRequest
             'password' => ['required', 'string', 'current_password'],
         ];
     }
-
-    public function withValidator($validator)
-    {
-
-        $validator->after(function($validator) {
-            if(empty($this->current_email)) return;
-
-            if($this->current_email != Auth::user()->email) {
-                $validator->errors()->add('current_email', 'The current mail specified is incorrect.');
-            }
-        });
-
-    }
 }
--- tests/Feature/Auth/ChangeEmailTest.php
+++ tests/Feature/Auth/ChangeEmailTest.php
@@ -7,8 +7,10 @@ use App\Models\User;
 use App\Notifications\Auth\UserEmailChangeConveyNotification;
 use App\Notifications\Auth\UserEmailChangeVerifyNotification;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Notification;
 use Laravel\Sanctum\Sanctum;
+use PHPUnit\Framework\Attributes\DataProvider;
 use Tests\TestCase;
 
 class ChangeEmailTest extends TestCase
@@ -25,6 +27,18 @@ class ChangeEmailTest extends TestCase
 
     private const CHANGE_EMAIL_ENDPOINT = 'api/auth/email';
 
+
+    public static function duplicateTesting(): iterable
+    {
+       yield 'The email has already been taken' => ['email'];
+       yield 'The pending email has already been taken' => ['pending_email'];
+    }
+
+    public static function requiredFieldsProvider(): iterable
+    {
+        yield 'pending email is required' => ['pending_email'];
+        yield 'password is required' => ['password'];
+    }
 
     /*
      * Happy path
@@ -67,4 +81,50 @@ class ChangeEmailTest extends TestCase
             return $notifiable->routes['mail'] === $newEmail;
         });
     }
+
+    public function test_user_cannot_change_password_incorrect()
+    {
+        $validPayload = $this->validPayload();
+         $user = User::factory()->create(['password' => 'strongPassword1234']);
+
+         Sanctum::actingAs($user);
+
+         $this->patchJson(self::CHANGE_EMAIL_ENDPOINT, $validPayload)
+             ->assertUnprocessable()->assertOnlyJsonValidationErrors('password');
+
+         User::query()->where('id', $user->id);
+
+         $this->assertTrue(Hash::check('strongPassword1234', $user->password));
+         $this->assertFalse(Hash::check($validPayload['password'], $user->password));
+    }
+
+    #[DataProvider('duplicateTesting')]
+    public function test_duplicateSample(string $verifiableField)
+    {
+        $validPayload = $this->validPayload();
+        $oldUser = User::factory()->create([$verifiableField => $validPayload['pending_email']]);
+
+        $user = User::factory()->create(['email' => 'anotherEmail@mail.com', 'password' => $validPayload['password']]);
+        Sanctum::actingAs($user);
+
+        $this->patchJson(self::CHANGE_EMAIL_ENDPOINT, $validPayload)
+            ->assertStatus(422)->assertOnlyJsonValidationErrors('pending_email');
+
+        $this->assertDatabaseMissing('users', ['id' => $user->id, 'email' => $validPayload['pending_email']]);
+        $this->assertDatabaseHas('users', ['id' => $oldUser->id, $verifiableField => $validPayload['pending_email']]);
+    }
+
+    #[DataProvider('requiredFieldsProvider')]
+    public function test_registration_requires_field(string $field): void
+    {
+        $payload = $this->validPayload();
+        Sanctum::actingAs(User::factory()->create(['password' =>  $payload['password']]));
+
+        unset($payload[$field]);
+
+        $this->patchJson(self::CHANGE_EMAIL_ENDPOINT, $payload)->assertUnprocessable()
+            ->assertOnlyJsonValidationErrors([$field]);
+
+    }
+
 }
