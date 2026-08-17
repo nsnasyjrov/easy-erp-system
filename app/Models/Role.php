cdacd5d453f0d91f6276d90ef15975a22978c816
Nail <nsnasyjrov@gmail.com>
2026-08-17 22:28:52 +0300

    chore: update cast

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Enums\RoleCode;
 use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
@@ -11,5 +12,12 @@ class Role extends Model
 {
     public function users(): HasMany {
         return $this->hasMany(User::class);
+    }
+
+    protected function casts(): array
+    {
+        return [
+            'code' => RoleCode::class,
+        ];
     }
 }
